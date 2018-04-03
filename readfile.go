ed92e100491c174135c6656f5bbc8eb54c4a678d
sunlong <sunlong@yixia.com>
2018-04-03 19:50:19 +0800

    文件读取

--- readfile.go
+++ readfile.go
@@ -3,21 +3,26 @@ package main
 import (
 	"os"
 	"fmt"
+	"io/ioutil"
 )
 
 func main(){
-	file := `./map1.go`
-	f,err := os.Open( file )	//open ./map1.go: no such file or directory 会报错
-	check( err )
-	content := make( []byte, 30 )
-	n1,err := f.Read( content )
-	fmt.Println( string( n1 ) )
-	f.Close()
-}
+	//读取所有文件
+	data, _ := ioutil.ReadFile( "./map.go" )
+	fmt.Println( string( data ) )
 
-
-func check( e error ){
-	if e != nil {
-		panic( e )
+	//只读取文件的一部分
+	f,err := os.Open( "./map.go" )	//open ./map1.go: no such file or directory 会报错
+	if err != nil {
+		fmt.Println( err )
+		os.Exit( 1 )
 	}
+
+	content := make( []byte, 10 )
+	n1,err := f.Read( content )
+	fmt.Printf( "%d bytes : %s \n", n1, string( content ) )
+	f.Close()
+
+	//读取行
+
 }
