bfe1f8c74eeeb712d327277cbb708236512f936e
sunlong <sunlong@yixia.com>
2018-05-07 19:59:05 +0800

    带超时的get请求

---
+++ get.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	//"os"
+	"io/ioutil"
+	"fmt"
+	"time"
+	//"os"
+	"os"
+)
+
+func main(){
+	//get 请求
+	url := "http://sl.test.com/api.php?name=sunlong"
+	client := &http.Client{ Timeout:1 * time.Second }
+	res, err := client.Get( url )
+	check( err )
+	res.Header.Set( "Content-Type", "application/x-www-form-urlencoded" )	//设置header
+
+	result, _ := ioutil.ReadAll( res.Body )	//返回beta
+	res.Body.Close()
+
+	//fmt.Printf( `%s`, result )
+	msg := string( result )
+	fmt.Println( msg )
+}
+
+func check( err error ){
+	defer func(){
+		if r := recover(); r != nil {
+			fmt.Println( r )
+			os.Exit( 1 )
+		}
+	}();
+
+	if err != nil {
+		panic( `request timeout` )
+	}
+}
--- panic.go
+++ panic.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main()  {
-	a()
+	//a()
 	b()
-	c()
+	//c()
 }
 
 func a(){
