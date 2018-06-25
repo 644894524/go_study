6153944ae2ffa09d6f2926fde667aa93666c8252
auto_deploy <auto_deploy@yixia.com>
2018-06-25 18:37:39 +0800

    tijiao

--- time.go
+++ time.go
@@ -6,12 +6,23 @@ import (
 )
 
 func main(){
-	stime := time.Now()
+	stime := time.Now()		//获取时间
 	fmt.Println( stime )
 	y, m, d := stime.Date()
 	h, min, s := stime.Clock()
+	fmt.Printf( "%d %02d %02d %02d:%02d:%02d \r\n", y, m, d, h, min, s )		//格式化时间
 
-	fmt.Printf( `%d %02d %02d %02d:%02d:%02d`, y, m, d, h, min, s )
+
+	//time.Sleep( 1000000 * time.Microsecond )		//毫秒
+	time.Sleep( 1 * time.Second )
+	fmt.Println( `haha` )
+
+
+	//时间戳
+	fmt.Println( stime.Unix() )
+
+
+	//前一个小时
 
 }
 
