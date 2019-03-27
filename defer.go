df05e625b9aec4208497d20de3bc95d3045df885
sunlong <sunlong@zhangyue.com>
2019-03-27 20:16:29 +0800

    错误处理和延迟函数

--- defer.go
+++ defer.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+func recoverName() {
+	if r := recover(); r!= nil {
+		fmt.Println("recovered from ", r)
+	}
+}
+
+
 func main(){
 	defer func() {
 		fmt.Println( "终止执行" )
 	}()
+
+	defer recoverName()
 
 	var arr = [...]int{ 1, 3, 5, 7, 9 }
 	for k, v := range arr {
@@ -13,4 +22,7 @@ func main(){
 	}
 
 
+	panic( "this is error" )
+
+	fmt.Println( "还会继续执行吗" )			//panic 终止程序，所以不会有输出了
 }
