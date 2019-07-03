2119be7892d2bafb666fa7f56483c4829955e033
sunlong <sun.long@immomo.com>
2019-07-03 15:37:12 +0800

    haha

--- tail.go
+++ tail.go
@@ -8,7 +8,6 @@ import (
 func main(){
 
 	t, err := tail.TailFile("/tmp/code_auto_push_0601.log", tail.Config{ Follow: true, Poll: true } )
-
 	if( err == nil ) {
 		for line := range t.Lines {
 			fmt.Println(line.Text)
